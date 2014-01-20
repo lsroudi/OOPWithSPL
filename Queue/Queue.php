f9053775c304475678f3a7becb6a5a4308c85e94
lsroudi <lsroudi@gmail.com>
2014-01-20 23:03:42 +0100

    how to use a SplQueue

---
+++ Queue/Queue.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Description of Queue
+ *
+ * (c) lsroudi <http://lsroudi.com/> <lsroudi@gmail.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+class Queue {
+    //put your code here
+}
+
+?>
