9bc94aca3e521af7173aa03003b690dcacbaeea1
lsroudi <lsroudi@gmail.com>
2014-01-22 16:24:56 +0100

    refactor and add unit test

---
+++ Queue/QueueTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Description of StackTest
+ *
+ * (c) lsroudi <http://lsroudi.com/> <lsroudi@gmail.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once 'Task.php';
+require_once 'Queue.php';
+
+class QueueTest extends \PHPUnit_Framework_TestCase {
+    
+    public function testExecute()
+    {        
+        $task1 = new Task('Task 1');
+        $task2 = new Task('Task 2');
+        $task3 = new Task('Task 3');
+
+        $queue = new Queue();
+        $queue->add(array($task1, $task2, $task3));
+
+        $resutlt = $queue->execute();
+
+
+        $this->assertEquals('task 1 executed', $resutlt);
+        
+        $this->assertEquals(2, $queue->count());
+        
+        
+    }
+
+}
+
+?>
