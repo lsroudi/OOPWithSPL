579c8bd0935eb7b6bff97b37f9fe65e7049305ac
lsroudi <lsroudi@gmail.com>
2014-01-23 19:41:53 +0100

    refactor and add autoloading with composer

--- Queue/Queue.php
+++ Queue/Queue.php
@@ -8,9 +8,10 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+namespace OOPWithSPL\Queue;
 
 
-class Queue extends SplQueue {
+class Queue extends \SplQueue {
 
     public function add($tasks)
     {
@@ -27,5 +28,25 @@ class Queue extends SplQueue {
     }
 
 }
+/**
+ * Example 
+require_once '../vendor/autoload.php';
+
+use OOPWithSPL\Queue\Task;
+
+$task1 = new Task('Task 1');
+$task2 = new Task('Task 2');
+$task3 = new Task('Task 3');
+
+
+$queue = new Queue();
+$queue->add(array($task1, $task2, $task3));
+
+echo $queue->execute(); // task 1 executed
+echo $queue->execute(); // task 2 executed
+echo $queue->execute(); // task 2 executed
+ */
+
+
 
 ?>
