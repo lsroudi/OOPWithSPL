21ac7c9919ee0c162b12ecb335a5f73214af9a96
lsroudi <lsroudi@gmail.com>
2014-01-24 23:18:25 +0100

    initial commit Spl Heap

---
+++ Heap/Heap.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Description of Heap
+ *
+ * (c) lsroudi <http://lsroudi.com/> <lsroudi@gmail.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+class Heap {
+    //put your code here
+}
+?>
+
