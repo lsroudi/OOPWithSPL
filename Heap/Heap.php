5dd188d85ac9114bb945a2b07cb97e4507f3c1d2
lsroudi <lsroudi@gmail.com>
2014-01-25 22:51:33 +0100

    add namespace and extend SplHeap

--- Heap/Heap.php
+++ Heap/Heap.php
@@ -8,8 +8,16 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-class Heap {
-    //put your code here
+
+namespace OOPWithSPL\Heap;
+
+class Heap extends \SplHeap {
+
+    protected function compare($value1, $value2)
+    {
+        return $value1 - $value2;
+    }
+
 }
 ?>
 
