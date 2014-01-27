426543781d7b7d89dfb7e10a623e68263b313c68
lsroudi <lsroudi@gmail.com>
2014-01-27 23:11:31 +0100

    add class MaxHeap

---
+++ Heap/MaxHeap.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Description of Heap
+ *
+ * (c) lsroudi <http://lsroudi.com/> <lsroudi@gmail.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace OOPWithSPL\Heap;
+
+class MaxHeap extends \SplMaxHeap {
+
+    protected function compare($value1, $value2)
+    {
+        
+    }
+
+}
+?>
+
