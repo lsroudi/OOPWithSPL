48e3eaab62330c2772fd82bc8b2175f9eaeb9623
lsroudi <lsroudi@gmail.com>
2014-02-06 17:21:21 +0100

    add unit test to test priorityLogger

---
+++ Priority/PriorityTest.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Description of PriorityTest
+ *
+ * (c) lsroudi <http://lsroudi.com/> <lsroudi@gmail.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once 'vendor/autoload.php';
+
+use OOPWithSPL\Priority\PriorityLogger;
+use OOPWithSPL\Priority\Logger;
+
+class PriorityTest extends PHPUnit_Framework_TestCase {
+
+
+    public function testPriority()
+    {
+        $priorityLogger = new PriorityLogger();
+
+        $logger = new Logger($priorityLogger);
+        $logger->addMessage('Message with notice type', Logger::NOTICE);
+        $logger->addMessage('Message with warning type', Logger::WARNING);
+        $logger->addMessage('Message with error type', Logger::ERROR);
+
+        $priorityLoggerQueue = $logger->getPriorityLogger();
+
+        $this->assertEquals('Message with error type', $priorityLoggerQueue->extract());
+        $this->assertEquals('Message with warning type', $priorityLoggerQueue->extract());
+        $this->assertEquals('Message with notice type', $priorityLoggerQueue->extract());
+
+
+    }
+
+}
+?>
+
