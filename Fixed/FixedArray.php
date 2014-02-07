67178ff00b6faee58abfe5e3df633ef05c4c4031
lsroudi <lsroudi@gmail.com>
2014-02-07 22:08:10 +0100

    update autoloading and create FixedArray class

---
+++ Fixed/FixedArray.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Description of FixedArray
+ *
+ * (c) lsroudi <http://lsroudi.com/> <lsroudi@gmail.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace OOPWithSPL\Fixed;
+
+class FixedArray extends \SplFixedArray {
+    //put your code here
+}
+?>
+
