f8f4ad22379f4fabc907cd8af33b7e540eaad59c
lsroudi <lsroudi@gmail.com>
2014-02-08 18:48:54 +0100

    add unit test for FixedArray

---
+++ Fixed/FixedTest.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Description of FixedTest
+ *
+ * (c) lsroudi <http://lsroudi.com/> <lsroudi@gmail.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+require 'vendor/autoload.php';
+
+use OOPWithSPL\Fixed\FixedArray;
+
+class FixedTest extends \PHPUnit_Framework_TestCase {
+
+    public function testFixedArray()
+    {
+        $fixedarray = new FixedArray(5);
+        $fixedarray[1] = "value 1";
+
+        $this->assertEquals(5, $fixedarray->count());
+        $this->assertEquals(5, count($fixedarray));   
+        $this->assertEquals(NULL, $fixedarray[0]); 
+        $this->assertEquals("value 1", $fixedarray[1]);          
+    }
+
+}
+?>
+
