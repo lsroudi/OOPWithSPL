2a78daaa69266064ad46c17ee116994f4c077f85
lsroudi <lsroudi@gmail.com>
2014-02-08 19:43:04 +0100

    update unit test to test fromArray method

--- Fixed/FixedTest.php
+++ Fixed/FixedTest.php
@@ -20,14 +20,25 @@ class FixedTest extends \PHPUnit_Framework_TestCase {
         $fixedarray[1] = "value 1";
 
         $this->assertEquals(5, $fixedarray->count());
-        $this->assertEquals(5, count($fixedarray));   
-        $this->assertEquals(NULL, $fixedarray[0]); 
-        $this->assertEquals("value 1", $fixedarray[1]);  
-        
+        $this->assertEquals(5, count($fixedarray));
+        $this->assertEquals(NULL, $fixedarray[0]);
+        $this->assertEquals("value 1", $fixedarray[1]);
+
         $fixedarray->setSize(10);
-        
-        $this->assertEquals("value 1", $fixedarray[1]); 
-        $this->assertEquals(10, count($fixedarray));          
+
+        $this->assertEquals("value 1", $fixedarray[1]);
+        $this->assertEquals(10, count($fixedarray));
+
+        $a = array('value 1', 'value 2', 7 => 'value 7');
+        $fixedarraythow = FixedArray::fromArray($a);
+
+        $this->assertEquals("value 1", $fixedarraythow[0]);
+        $this->assertEquals("value 2", $fixedarraythow[1]);
+        $this->assertEquals(NULL, $fixedarraythow[2]);
+        $this->assertEquals(8, count($fixedarraythow));
+
+        $fixedarraytree = FixedArray::fromArray($a,false);
+        $this->assertEquals(3, count($fixedarraytree));        
     }
 
 }
