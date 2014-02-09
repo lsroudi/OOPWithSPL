77ecdf091fc8ae2f2de38c52eef58bc46618de8f
lsroudi <lsroudi@gmail.com>
2014-02-09 20:59:04 +0100

    create ObjectStorage class

---
+++ ObjectStorage/ObjectStorage.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Description of ObjectStorage
+ *
+ * (c) lsroudi <http://lsroudi.com/> <lsroudi@gmail.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace OOPWithSPL\ObjectStorage;
+
+class ObjectStorage extends \SplObjectStorage {
+    //put your code here
+}
+?>
+
