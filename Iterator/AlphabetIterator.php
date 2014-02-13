8dd2358ea8981349f8e98b575ab1ad805833c64f
lsroudi <lsroudi@gmail.com>
2014-02-13 19:51:32 +0100

    add namespace

--- Iterator/AlphabetIterator.php
+++ Iterator/AlphabetIterator.php
@@ -8,6 +8,8 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+namespace OOPWithSPL\Iterator;
+
 class AlphabetIterator implements Iterator {
 
     protected $current;
