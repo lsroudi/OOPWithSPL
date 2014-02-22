67e1f578534499aa1b947d01e83431ff2c5aab17
lsroudi <lsroudi@gmail.com>
2014-02-22 23:03:54 +0100

    create CachingIterator class

---
+++ Iterator/Caching/CachingIterator.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Description of CachingIterator
+ *
+ * (c) lsroudi <http://lsroudi.com/> <lsroudi@gmail.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace OOPSPL\Iterator\Caching;
+
+class CachingIterator extends \CachingIterator {
+    
+}
+?>
+
