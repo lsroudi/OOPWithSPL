1ab87cf42b536ac90b1dcf04c714a9e693d27ac1
lsroudi <lsroudi@gmail.com>
2014-04-08 23:13:09 +0200

    add namespace

--- Iterator/CallbackFilterIterator.php
+++ Iterator/CallbackFilterIterator.php
@@ -8,6 +8,8 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+namespace OOPSPL\Iterator;
+
 class CallbackFilterIterator {
     //put your code here
 }
